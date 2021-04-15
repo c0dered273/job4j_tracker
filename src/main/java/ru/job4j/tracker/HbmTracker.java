7ad88733c287ef2b9b9264a5c9d39958466baaeb
c0dered273 <okaschuk@gmail.com>
2021-04-15 11:38:51 +0300

    HbmTracker. [#458168]

--- src/main/java/ru/job4j/tracker/HbmTracker.java
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -1,14 +1,18 @@
 package ru.job4j.tracker;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 public class HbmTracker implements Store, AutoCloseable {
+    private static final Logger logger = LoggerFactory.getLogger(HbmTracker.class);
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
             .configure().build();
     private final SessionFactory sf = new MetadataSources(registry)
@@ -21,68 +25,112 @@ public class HbmTracker implements Store, AutoCloseable {
 
     @Override
     public Item add(Item item) {
+        Transaction tx = null;
         try (Session session = sf.openSession()) {
-            session.beginTransaction();
+            tx = session.beginTransaction();
             session.save(item);
-            session.getTransaction().commit();
+            tx.commit();
+        } catch (HibernateException e) {
+            logger.error("Error in add() with Item={}", item, e);
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
         }
         return item;
     }
 
     @Override
     public boolean replace(Integer id, Item item) {
+        boolean result = false;
+        Transaction tx = null;
         try (Session session = sf.openSession()) {
-            session.beginTransaction();
+            tx = session.beginTransaction();
             session.update(item);
-            session.getTransaction().commit();
+            tx.commit();
+            result = true;
+        } catch (HibernateException e) {
+            logger.error("Error in replace() with Item={}", item, e);
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
         }
-        return true;
+        return result;
     }
 
     @Override
     public boolean delete(Integer id) {
+        boolean result = false;
+        Transaction tx = null;
         try (Session session = sf.openSession()) {
-            session.beginTransaction();
+            tx = session.beginTransaction();
             Item item = new Item();
             item.setId(id);
             session.delete(item);
-            session.getTransaction().commit();
+            tx.commit();
+            result = true;
+        } catch (HibernateException e) {
+            logger.error("Error in delete() with Item.id={}", id, e);
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
         }
-        return true;
+        return result;
     }
 
     @Override
     public List<Item> findAll() {
-        List rsl;
+        List result = null;
+        Transaction tx = null;
         try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            rsl = session.createQuery("from ru.job4j.tracker.Item").list();
-            session.getTransaction().commit();
+            tx = session.beginTransaction();
+            Item item = new Item();
+            result = session.createQuery("from ru.job4j.tracker.Item").list();
+            session.delete(item);
+            tx.commit();
+        } catch (HibernateException e) {
+            logger.error("Error in findAll()", e);
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
         }
-        return rsl;
+        return result;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        List rsl;
+        List result = null;
+        Transaction tx = null;
         try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            rsl = session.createQuery("from ru.job4j.tracker.Item i where i.name = :name")
+            tx = session.beginTransaction();
+            Item item = new Item();
+            result = session.createQuery("from ru.job4j.tracker.Item i where i.name = :name")
                     .setParameter("name", key).list();
-            session.getTransaction().commit();
+            session.delete(item);
+            tx.commit();
+        } catch (HibernateException e) {
+            logger.error("Error in findByName() with Item.name={}", key, e);
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
         }
-        return rsl;
+        return result;
     }
 
     @Override
     public Item findById(Integer id) {
-        Item rsl;
+        Item result = null;
+        Transaction tx = null;
         try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            rsl = session.get(Item.class, id);
-            session.getTransaction().commit();
+            tx = session.beginTransaction();
+            result = session.get(Item.class, id);
+            tx.commit();
+        } catch (HibernateException e) {
+            logger.error("Error in findById() with Item.id={}", id, e);
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
         }
-        return rsl;
+        return result;
     }
 
     @Override
